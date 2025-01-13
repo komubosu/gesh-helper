27bb9194b642a82d3a7ba396a3fe5108eaed5d6d
komubosu <redisooon@ya.ru>
2025-01-13 13:43:19 +0700

    refactor: update GitHub Actions workflow for deployment; remove Dockerfile and adjust Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  basePath: '/gesh-helper',
-  assetPrefix: '/gesh-helper',
-  trailingSlash: true,
-  output: 'standalone',
+  output: 'export',
 };
 
 export default nextConfig;
