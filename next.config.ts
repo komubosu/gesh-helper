7030c8caeece0d2680dad6678af9d99645e337d6
komubosu <redisooon@ya.ru>
2025-01-13 15:50:59 +0700

    refactor: update Next.js configuration and package dependencies; modify asset prefix, adjust dev script, and upgrade packages

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/gesh-helper',
-  assetPrefix: '/gesh-helper',
 };
 
 export default nextConfig;
