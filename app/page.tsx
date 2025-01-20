94b544415c4e65b2caed502e9a0d7faa39ce7d7d
komubosu <redisooon@ya.ru>
2025-01-20 14:31:40 +0700

    refactor: remove unused pages and layouts; clean up site configuration and update main page structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,69 @@
-import { Code } from '@heroui/code';
-import { Link } from '@heroui/link';
-import { Snippet } from '@heroui/snippet';
-import { button as buttonStyles } from '@heroui/theme';
+import { cn } from '@/shared/lib/cn';
 
-import { siteConfig } from '@/shared/config/site';
-import { GithubIcon } from '@/shared/ui/icons';
-import { subtitle, title } from '@/shared/ui/primitives';
+const data = {
+  sector: [
+    {
+      type: 'sector',
+      value: {
+        name_sector: 'Сектор А',
+        lifts: [
+          {
+            name: 'Кресельная КД «Фристайл»',
+            is_work: true,
+          },
+          {
+            name: 'Кресельная КД «Фристайл2»',
+            is_work: false,
+          },
+        ],
+      },
+      id: '37bdc96c-032b-413e-adb1-f04a78fe13d1',
+    },
+    {
+      type: 'sector',
+      value: {
+        name_sector: 'Сектор В',
+        lifts: [
+          {
+            name: 'Кресельная КД «Булочка»',
+            is_work: false,
+          },
+        ],
+      },
+      id: 'be51d88b-fa87-4db5-8fa7-b2176d9dbfb1',
+    },
+  ],
+};
 
 export default function Home() {
+  console.log(data.sector);
+
   return (
     <section className='flex flex-col items-center justify-center gap-4 py-8 md:py-10'>
-      <div className='inline-block max-w-xl justify-center text-center'>
+      {/* <div className='inline-block max-w-xl justify-center text-center'>
         <span className={title()}>Make&nbsp;</span>
         <span className={title({ color: 'violet' })}>beautiful&nbsp;</span>
         <br />
         <span className={title()}>websites regardless of your design experience.</span>
         <div className={subtitle({ class: 'mt-4' })}>Beautiful, fast and modern React UI library.</div>
-      </div>
+      </div> */}
 
-      <div className='flex gap-3'>
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: 'primary',
-            radius: 'full',
-            variant: 'shadow',
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: 'bordered', radius: 'full' })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
-
-      <div className='mt-8'>
-        <Snippet hideCopyButton hideSymbol variant='bordered'>
-          <span>
-            Get started by editing <Code color='primary'>app/page.tsx</Code>
-          </span>
-        </Snippet>
+      <div>
+        {data.sector.map(sector => (
+          <div key={sector.id}>
+            <h2 className='font-bold text-lg'>{sector.value.name_sector}</h2>
+            <ul className='pl-4'>
+              {sector.value.lifts.map((lift, index) => (
+                <li key={index} className='flex gap-2'>
+                  <span>{lift.name}</span>
+                  <span className={cn(lift.is_work ? 'text-green-500' : 'text-red-500')}>
+                    {lift.is_work ? 'Работает' : 'Не работает'}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </section>
   );
